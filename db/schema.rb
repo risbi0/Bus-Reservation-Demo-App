868773271876b4d75f868191eccc64ec000be950
teduniq <reyestedowen3@gmail.com>
2021-11-09 00:53:20 +0800

    updated seeds and schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_065433) do
+ActiveRecord::Schema.define(version: 2021_11_08_162455) do
 
   create_table "schedules", force: :cascade do |t|
     t.string "departure", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@
 puts "Seeding..."
 
 Schedule.create(departure: 'Manila', destination: 'Quezon City', date_time: '2021-12-21 12:30', seats_available: 45, price: 120)
-Schedule.create(departure: 'Pateros', destination: 'Pasig City', date_time: DateTime.parse('2021-12-22 13:20'), seats_available: 45, price: 80)
-Schedule.create(departure: 'Makati City', destination: 'Taguig City', date_time: DateTime.parse('2021-12-25 14:45'), seats_available: 45, price: 90)
-Schedule.create(departure: 'Manila', destination: 'Pasig City', date_time: DateTime.parse('2021-12-27 11:00'), seats_available: 45, price: 100)
-Schedule.create(departure: 'Marikina City', destination: 'Makati City', date_time: DateTime.parse('2021-12-23 10:00'), seats_available: 40, price: 100)
-Schedule.create(departure: 'Makati City', destination: 'Taguig City', date_time: DateTime.parse('2021-12-23 04:00'), seats_available: 40, price: 70)
-Schedule.create(departure: 'Quezon City', destination: 'Manila', date_time: DateTime.parse('2021-12-25 13:00'), seats_available: 40, price: 60)
-Schedule.create(departure: 'Quezon City', destination: 'Makati City', date_time: DateTime.parse('2021-12-26 18:00'), seats_available: 40, price: 130)
-Schedule.create(departure: 'Quezon City', destination: 'Marikina City', date_time: DateTime.parse('2021-12-28 08:00'), seats_available: 35, price: 150)
-Schedule.create(departure: 'Manila', destination: 'Marikina City', date_time: DateTime.parse('2021-12-31 08:30'), seats_available: 35, price: 85)
-Schedule.create(departure: 'Pasig City', destination: 'Manila', date_time: DateTime.parse('2021-12-28 20:10'), seats_available: 35, price: 95)
-Schedule.create(departure: 'Pateros', destination: 'Manila', date_time: DateTime.parse('2021-12-31 23:45'), seats_available: 35, price: 130)
+Schedule.create(departure: 'Pateros', destination: 'Pasig City', date_time: '2021-12-22 13:20', seats_available: 45, price: 80)
+Schedule.create(departure: 'Makati City', destination: 'Taguig City', date_time: '2021-12-25 14:45', seats_available: 45, price: 90)
+Schedule.create(departure: 'Manila', destination: 'Pasig City', date_time: '2021-12-27 11:00', seats_available: 45, price: 100)
+Schedule.create(departure: 'Marikina City', destination: 'Makati City', date_time: '2021-12-23 10:00', seats_available: 40, price: 100)
+Schedule.create(departure: 'Makati City', destination: 'Taguig City', date_time: '2021-12-23 04:00', seats_available: 40, price: 70)
+Schedule.create(departure: 'Quezon City', destination: 'Manila', date_time: '2021-12-25 13:00', seats_available: 40, price: 60)
+Schedule.create(departure: 'Quezon City', destination: 'Makati City', date_time: '2021-12-26 18:00', seats_available: 40, price: 130)
+Schedule.create(departure: 'Quezon City', destination: 'Marikina City', date_time: '2021-12-28 08:00', seats_available: 35, price: 150)
+Schedule.create(departure: 'Manila', destination: 'Marikina City', date_time: '2021-12-31 08:30', seats_available: 35, price: 85)
+Schedule.create(departure: 'Pasig City', destination: 'Manila', date_time: '2021-12-28 20:10', seats_available: 35, price: 95)
+Schedule.create(departure: 'Pateros', destination: 'Manila', date_time: '2021-12-31 23:45', seats_available: 35, price: 130)
 
 puts "Seeding done."

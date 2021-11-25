4780c10cc1ccab959f1671d86ff5a2aed4b547c0
teduniq <reyestedowen3@gmail.com>
2021-11-25 12:38:52 +0800

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,21 +8,25 @@
 
 puts "Seeding..."
 
-Schedule.create(departure: 'Manila', destination: 'Quezon City', date: '2021-12-21', time: '12:30', seats_available: 45, price: 120)
-Schedule.create(departure: 'Pateros', destination: 'Pasig City', date: '2021-12-22', time: '13:20', seats_available: 45, price: 80)
-Schedule.create(departure: 'Makati City', destination: 'Taguig City', date: '2021-12-25', time: '14:45', seats_available: 45, price: 90)
-Schedule.create(departure: 'Manila', destination: 'Pasig City', date: '2021-12-27', time: '11:00', seats_available: 45, price: 100)
-Schedule.create(departure: 'Marikina City', destination: 'Makati City', date: '2021-12-23', time: '10:00', seats_available: 40, price: 100)
-Schedule.create(departure: 'Makati City', destination: 'Taguig City', date: '2021-12-23', time: '04:00', seats_available: 40, price: 70)
-Schedule.create(departure: 'Quezon City', destination: 'Manila', date: '2021-12-25', time: '13:00', seats_available: 40, price: 60)
-Schedule.create(departure: 'Quezon City', destination: 'Makati City', date: '2021-12-26', time: '18:00', seats_available: 40, price: 130)
-Schedule.create(departure: 'Quezon City', destination: 'Marikina City', date: '2021-12-28', time: '08:00', seats_available: 35, price: 150)
-Schedule.create(departure: 'Manila', destination: 'Marikina City', date: '2021-12-31', time: '08:30', seats_available: 35, price: 85)
-Schedule.create(departure: 'Pasig City', destination: 'Manila', date: '2021-12-28', time: '20:10', seats_available: 35, price: 95)
-Schedule.create(departure: 'Pateros', destination: 'Manila', date: '2021-12-31', time: '23:45', seats_available: 35, price: 130)
-Schedule.create(departure: 'Pasig City', destination: 'Quezon City', date: '2021-11-16', time: '23:45', seats_available: 35, price: 150)
-Schedule.create(departure: 'Makati City', destination: 'Manila', date: '2021-11-15', time: '23:45', seats_available: 35, price: 140)
-Schedule.create(departure: 'Manila', destination: 'Pasig City', date: '2021-11-14', time: '23:45', seats_available: 35, price: 170)
+Booking.delete_all
+Schedule.delete_all
+Seating.delete_all
+
+Schedule.create(departure: 'Manila', destination: 'Quezon City', date: '2021-12-21', time: '12:30', seats_available: 47, price: 120).update_column(:id, 1)
+Schedule.create(departure: 'Pateros', destination: 'Pasig City', date: '2021-12-22', time: '13:20', seats_available: 47, price: 80).update_column(:id, 2)
+Schedule.create(departure: 'Makati City', destination: 'Taguig City', date: '2021-12-25', time: '14:45', seats_available: 47, price: 90).update_column(:id, 3)
+Schedule.create(departure: 'Manila', destination: 'Pasig City', date: '2021-12-27', time: '11:00', seats_available: 47, price: 100).update_column(:id, 4)
+Schedule.create(departure: 'Marikina City', destination: 'Makati City', date: '2021-12-23', time: '10:00', seats_available: 47, price: 100).update_column(:id, 5)
+Schedule.create(departure: 'Makati City', destination: 'Taguig City', date: '2021-12-23', time: '04:00', seats_available: 47, price: 70).update_column(:id, 6)
+Schedule.create(departure: 'Quezon City', destination: 'Manila', date: '2021-12-25', time: '13:00', seats_available: 47, price: 60).update_column(:id, 7)
+Schedule.create(departure: 'Quezon City', destination: 'Makati City', date: '2021-12-26', time: '18:00', seats_available: 47, price: 130).update_column(:id, 8)
+Schedule.create(departure: 'Quezon City', destination: 'Marikina City', date: '2021-12-28', time: '08:00', seats_available: 47, price: 150).update_column(:id, 9)
+Schedule.create(departure: 'Manila', destination: 'Marikina City', date: '2021-12-31', time: '08:30', seats_available: 47, price: 85).update_column(:id, 10)
+Schedule.create(departure: 'Pasig City', destination: 'Manila', date: '2021-12-28', time: '20:10', seats_available: 47, price: 95).update_column(:id, 11)
+Schedule.create(departure: 'Pateros', destination: 'Manila', date: '2021-12-31', time: '23:45', seats_available: 47, price: 130).update_column(:id, 12)
+Schedule.create(departure: 'Pasig City', destination: 'Quezon City', date: '2021-11-16', time: '23:45', seats_available: 47, price: 150).update_column(:id, 13)
+Schedule.create(departure: 'Makati City', destination: 'Manila', date: '2021-11-15', time: '23:45', seats_available: 47, price: 140).update_column(:id, 14)
+Schedule.create(departure: 'Manila', destination: 'Pasig City', date: '2021-11-14', time: '23:45', seats_available: 47, price: 170).update_column(:id, 15)
 
 15.times { |i|
     Seating.create(schedule_id: i + 1,  one: true, two: true, three: true, four: true, five: true, six: true, seven: true, eight: true, nine: true, ten: true,

7f09b6a0ee8411b55dba8bdfa97afe6ff7ae6dde
teduniq <reyestedowen3@gmail.com>
2021-11-30 16:27:32 +0800

    fix heroku background image not showing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,8 +27,11 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Do fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = true
+
+  # Enable Rails's static asset server
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = 'http://assets.example.com'
